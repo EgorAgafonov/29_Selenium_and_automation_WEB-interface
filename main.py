e6c7e0791836dbc3d85099ac5ce9d1425b81e674
Yegoriktoporik <agafonov.egor.1985@gmail.com>
2023-06-23 10:23:51 +0300

    Files updated

--- main.py
+++ main.py
@@ -1,12 +1,11 @@
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
-from selenium.webdriver.common.proxy import Proxy
 from selenium.webdriver.common.action_chains import ActionChains
 
 driver = webdriver.Chrome()
 driver.get('https://petfriends.skillfactory.ru/new_user')
-ID_name = driver.find_element(By.ID, 'name')
+request_ID_value = driver.find_element(By.ID, 'name')
 actions = ActionChains(driver)
 actions.click(on_element=None)
 
