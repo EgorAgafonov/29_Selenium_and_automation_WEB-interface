0f93308394fc382e0aac4af33627f4c529784709
Yegoriktoporik <agafonov.egor.1985@gmail.com>
2023-06-23 17:35:21 +0300

    Files updated

--- tests/test_selenium_simple.py
+++ tests/test_selenium_simple.py
@@ -1,22 +1,25 @@
 from selenium import webdriver
-from selenium.webdriver.common import by
+from selenium.webdriver.common.by import By
 import time
 
-driver = webdriver.Chrome()
-driver.get("https://petfriends.skillfactory.ru")
-title = driver.title
 
+def test_search_example(selenium):
+    """ Search some phrase in google and make a screenshot of the page. """
 
+    selenium.get("https://google.com")
 
+    time.sleep(5)
 
+    search_input = selenium.find_element(By.NAME, "q")
+    search_input.clear()
+    search_input.send_keys('first test')
 
-# def test_search_example(selenium):
-#     """ Search some phrase in google and make a screenshot of the page. """
-#
-#     # Open google search page:
-#     selenium.get('https://google.com')
-#
-#     time.sleep(5)  # just for demo purposes, do NOT repeat it on real projects!
-#
-#     # Find the field for search text input:
-#     search_input = selenium.
+    time.sleep(5)
+
+    search_button = selenium.find_element(By.NAME, "btnK")
+    search_button.submit()
+
+    time.sleep(10)
+
+    selenium.save_screenshot('result.png')
+
