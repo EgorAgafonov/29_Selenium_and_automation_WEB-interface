dc3855d729107308833e309c188193c77b2d1325
Yegoriktoporik <agafonov.egor.1985@gmail.com>
2023-06-26 12:27:22 +0300

    Files updated

--- tests/test_selenium_petfriends.py
+++ tests/test_selenium_petfriends.py
@@ -1,39 +1,43 @@
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common import keys
 import time
 
 # 2
+
 
 def test_petfriends(selenium):
     # Open PetFriends base page:
     selenium.get("https://petfriends.skillfactory.ru/")
 
-    time.sleep(5)  # just for demo purposes, do NOT repeat it on real projects!
+    time.sleep(3)  # just for demo purposes, do NOT repeat it on real projects!
 
     # click on the new user button
-    btn_new_user = selenium.find_element_by_xpath("//button[@onclick=\"document.location='/new_user';\"]")
+    btn_new_user = selenium.find_element (By.XPATH, '//button[@onclick=\"document.location='/new_user';\"]')
 
     btn_new_user.click()
 
     # click existing user button
-    btn_exist_acc = selenium.find_element_by_link_text(u"У меня уже есть аккаунт")
+    btn_exist_acc = selenium.find_element(By.LINK_TEXT, "У меня уже есть аккаунт")
     btn_exist_acc.click()
 
     # add email
-    field_email = selenium.find_element_by_id("email")
+    field_email = selenium.find_element(By.ID, "email")
     field_email.clear()
-    field_email.send_keys("<your_email>")
+    field_email.send_keys("fonovagafonov@yandex.ru")
 
     # add password
-    field_pass = selenium.find_element_by_id("pass")
+    field_pass = selenium.find_element (By.ID, "pass")
     field_pass.clear()
-    field_pass.send_keys("<your_password>")
+    field_pass.send_keys("123456")
 
     # click submit button
-    btn_submit = selenium.find_element_by_xpath("//button[@type='submit']")
+    btn_submit = selenium.find_element(By.XPATH, "//button[@type='submit']")
     btn_submit.click()
 
-    time.sleep(10)  # just for demo purposes, do NOT repeat it on real projects!
+    time.sleep(3)  # just for demo purposes, do NOT repeat it on real projects!
     if selenium.current_url == 'https://petfriends.skillfactory.ru/all_pets':
         # Make the screenshot of browser window:
         selenium.save_screenshot('result_petfriends.png')
     else:
-        raise Exception("login error")
+        raise Exception("Login error")
--- tests/test_selenium_simple.py
+++ tests/test_selenium_simple.py
@@ -2,25 +2,33 @@ from selenium import webdriver
 from selenium.webdriver.common.by import By
 import time
 
+
 # 1
 
 def test_search_example(selenium):
     """ Search some phrase in google and make a screenshot of the page. """
 
-    selenium.get("https://google.com")
 
-    time.sleep(3)
+    selenium.get("https://google.com")  # - метод get загружает страницу по указанному адресу
 
-    search_input = selenium.find_element(By.NAME, "q")
-    search_input.clear()
-    search_input.send_keys("qwe")
+    time.sleep(3)  # - метод sleep дает задержку выполнения следующей строки кода (команды) для целей полной загрузки
+    # страницы перед отправкой следующей команды
+
+    search_input = selenium.find_element(By.NAME, "q")  # поиск элемента по имени (тэгу) на странице с помощью метода
+    # .find_element(By.NAME, "element's name")
+    search_input.clear()  # очистка текста, если метод clear применяется к элементу для ввода текста (поле ввода)
+    search_input.send_keys("qwlertqqweeevxcvxcv") # симуляция печати клавишами букв "qwe" методом .send_keys для ввода
+    # текста в поле поиска
 
     time.sleep(3)
 
     search_button = selenium.find_element(By.NAME, "btnK")
     search_button.submit()
 
-    time.sleep(10)
+    time.sleep(3)
 
     selenium.save_screenshot('result.png')
 
+selenium = webdriver.Chrome
+search_button = selenium.find_element(By.NAME, "btnK")
+search_button.click()
