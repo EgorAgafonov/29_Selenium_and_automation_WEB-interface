a384d2cc9caedd481924a1c39baebbfa92e70ed7
Yegoriktoporik <agafonov.egor.1985@gmail.com>
2023-06-29 18:11:06 +0300

    Files updated

--- tests/test_PetFriends_waits.py
+++ tests/test_PetFriends_waits.py
@@ -11,6 +11,9 @@ driver = webdriver.Chrome('tests/chromedriver.exe')
 driver.get('http://petfriends.skillfactory.ru/login')
 
 
+
+                                                 # Неявные ожидания
+
 def test_implicitly_wait_my_pets():
     """Блок тестов implicitly_wait (неявные ожидания) на основе библиотеки Selenium для проверки загрузки элементов
     карточки питомца на платформе http://petfriends.skillfactory.ru."""
@@ -42,53 +45,49 @@ def test_implicitly_wait_my_pets():
     driver.get('https://petfriends.skillfactory.ru/all_pets')
     pet_age = driver.find_element(By.CSS_SELECTOR, '.card-deck .card-text')
     assert pet_age.text != ''
-#
-#
-# def test_explicit_wait_my_pets():
-#     """Блок тестов explicit_wait (явные ожидания) на предмет загрузки множества элементов страницы
-#     (проверка таблицы питомцев) на платформе http://petfriends.skillfactory.ru."""
-#
-#     driver.find_element(By.ID, "email").send_keys('fonovagafonov@yandex.ru')
-#     time.sleep(2)
-#     driver.find_element(By.ID, "pass").send_keys('123456')
-#     time.sleep(2)
-#     driver.find_element(By.CSS_SELECTOR, 'button[type="submit"]').click()
-#     assert driver.find_element(By.TAG_NAME, "h1").text == "PetFriends"
-#
-#     # 1. ВНИМАНИЕ! Тест валиден только в случае, если все карточки питомцев на сайте имеют загруженное фото!
-#     # С помощью класса WebDriverWait и метода presence_of_all_elements_located () выполним процедуру явного ожидания
-#     # загрузки на странице множества элементов с фото питомцев и определим переменную для их хранения. После чего с
-#     # помощью цикла for in range и инструкции assert проверим полученные элементы на предмет наличия в них обязательного
-#     # атрибута 'src'.
-#
-#     pets_photos = WebDriverWait(driver, 2).until(EC.presence_of_all_elements_located((By.CSS_SELECTOR,
-#     'div:nth-of-type(2) > div > div')))
-#
-#     for photo in range(len(pets_photos)):
-#         assert pets_photos[photo].get_attribute('src') != ''
-#
-#
-#     # 2. ВНИМАНИЕ ! Тест валиден только в случае, если у всего списка питомцев на сайте в карточках указано имя!
-#     # С помощью класса WebDriverWait и метода presence_of_all_elements_located () выполним процедуру явного ожидания
-#     # загрузки на странице множества элементов с именами питомцев и определим переменную для их хранения. После чего с
-#     # помощью цикла for in range и инструкции assert проверим полученные элементы на предмет наличия в них атрибутов с
-#     # текстом (поля с именами питомцев не пустые).
-#
-#     pets_names = WebDriverWait(driver, 2).until(EC.presence_of_all_elements_located((By.CSS_SELECTOR,
-#     '.card-deck .card-title')))
-#
-#     for name in range(len(pets_names)):
-#         assert pets_names[name].text != ''
-#
-#
-#     # 3. ВНИМАНИЕ ! Тест валиден только в случае, если у всего списка питомцев на сайте в карточках указан возраст!
-#     # С помощью класса WebDriverWait и метода presence_of_all_elements_located () выполним процедуру явного ожидания
-#     # загрузки на странице множества элементов с указанным возрастом питомцев и определим переменную для их хранения.
-#     # После чего с помощью цикла for in range и инструкции assert проверим полученные элементы на предмет наличия в
-#     # них атрибутов с указанным возрастом питомца (поля с возрастом питомцев не пустые).
-#
-#     pets_age = WebDriverWait(driver, 2).until(EC.presence_of_all_elements_located((By.CSS_SELECTOR,
-#     '.card-deck .card-text')))
-#
-#     for age in range(len(pets_age)):
-#         assert pets_age[age].text != ''
+
+
+                                                # Явные ожидания
+
+def test_explicit_wait_my_pets():
+    """Блок тестов explicit_wait (явные ожидания) на предмет загрузки множества элементов страницы
+    (проверка таблицы питомцев) на платформе http://petfriends.skillfactory.ru."""
+
+    driver.find_element(By.ID, "email").send_keys('указать зарегистрированный email')
+    time.sleep(2)
+    driver.find_element(By.ID, "pass").send_keys('указать зарегистрированный password')
+    time.sleep(2)
+    driver.find_element(By.CSS_SELECTOR, 'button[type="submit"]').click()
+    assert driver.find_element(By.TAG_NAME, "h1").text == "PetFriends"
+
+    # 1. ВНИМАНИЕ! Тест валиден только в случае, если все карточки питомцев на сайте имеют загруженное фото!
+    # С помощью класса WebDriverWait и метода presence_of_all_elements_located () выполним процедуру явного ожидания
+    # загрузки на странице множества элементов с фото питомцев и определим переменную для их хранения. После чего с
+    # помощью цикла for in range и инструкции assert проверим полученные элементы на предмет наличия в них обязательного
+    # атрибута 'src'.
+    pets_photos = WebDriverWait(driver, 2).until(EC.presence_of_all_elements_located((By.CSS_SELECTOR,
+    '.card-deck .card-img-top')))
+    for photo in range(len(pets_photos)):
+        assert pets_photos[photo].get_attribute('src') != ''
+
+
+    # 2. ВНИМАНИЕ ! Тест валиден только в случае, если у всего списка питомцев на сайте в карточках указано имя!
+    # С помощью класса WebDriverWait и метода presence_of_all_elements_located () выполним процедуру явного ожидания
+    # загрузки на странице множества элементов с именами питомцев и определим переменную для их хранения. После чего с
+    # помощью цикла for in range и инструкции assert проверим полученные элементы на предмет наличия в них атрибутов с
+    # текстом (поля с именами питомцев не пустые).
+    pets_names = WebDriverWait(driver, 2).until(EC.presence_of_all_elements_located((By.CSS_SELECTOR,
+    '.card-deck .card-title')))
+    for name in range(len(pets_names)):
+        assert pets_names[name].text != ''
+
+
+    # 3. ВНИМАНИЕ ! Тест валиден только в случае, если у всего списка питомцев на сайте в карточках указан возраст!
+    # С помощью класса WebDriverWait и метода presence_of_all_elements_located () выполним процедуру явного ожидания
+    # загрузки на странице множества элементов с указанным возрастом питомцев и определим переменную для их хранения.
+    # После чего с помощью цикла for in range и инструкции assert проверим полученные элементы на предмет наличия в
+    # них атрибутов с указанным возрастом питомца (поля с возрастом питомцев не пустые).
+    pets_age = WebDriverWait(driver, 2).until(EC.presence_of_all_elements_located((By.CSS_SELECTOR,
+    '.card-deck .card-text')))
+    for age in range(len(pets_age)):
+        assert pets_age[age].text != ''
